d4ebc371513b5ba5b20497bfdfeb17c0924bcb69
mostafa0x <dodomido12350@gmail.com>
2025-06-18 05:28:11 -0700

    init commit

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import { Stack } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }} />;
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
-import './global.css';
+import '../global.css';
 
-export default function App() {
+export default function Home() {
   return (
     <>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
